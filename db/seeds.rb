c73cdf2a4cd8139fc7a35ec87ef3b816f3a022b8
Igor Descayrac <igor.descayrac@gmail.com>
2022-02-02 17:06:27 +0100

    Seed database from CSV

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,25 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+require 'csv'
+
+csv_text = File.read(Rails.root.join('lib', 'seeds', 'pokemon.csv'))
+csv = CSV.parse(csv_text, :headers => true, :encoding => 'ISO-8859-1')
+
+csv.each do |row|
+  p = Pokemon.new
+  p.number = row['#']
+  p.name = row['Name']
+  p.type1 = row['Type 1']
+  p.type2 = row['Type 2']
+  p.total = row['Total']
+  p.hp = row['HP']
+  p.attack = row['Attack']
+  p.defense = row['Defense']
+  p.spAtk = row['Sp. Atk']
+  p.spDef = row['Sp. Def']
+  p.speed = row['Speed']
+  p.generation = row['Generation']
+  p.legendary = row['Legendary']
+  p.save
+  puts "#{p.number}, #{p.name} saved"
+end
+
+puts "There are now #{Pokemon.count} rows in the pokemon table"
